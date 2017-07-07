9f5bd03c023213f8bea9088a801feb165e89674d
sam <sam@localhost>
2017-07-07 17:47:08 +0200

    Set window title for i3 to behave itself

--- desktop/src/com/underwatch/game/desktop/DesktopLauncher.java
+++ desktop/src/com/underwatch/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.underwatch.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Underwatch";
 		new LwjglApplication(new Game(), config);
 	}
 }
